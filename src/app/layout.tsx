9051366d9767b5fa1a892cefe830b0b653b165ec
Thales <90907393+thalesamaral@users.noreply.github.com>
2025-05-02 23:07:47 -0300

    initial

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+import { Toaster } from "@/components/ui/sonner";
+
+import { CartProvider } from "./[slug]/menu/contexts/cart";
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "FSW Food",
+  description: "Bora finalizar esse projeto lindo!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>
+        <CartProvider>{children}</CartProvider>
+
+        <Toaster />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+
+const HomePage: FC = () => {
+  return <h1>Hello</h1>;
+}
+
+export default HomePage;
+
+//  http://localhost:3000/fsw-food
+//  http://localhost:3000/fsw-food/menu?consumptionMethod=dine_in
